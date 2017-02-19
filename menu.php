4c2dc47ea4237b7cedc4682f0d16c485a33599b6
edaym <mailmike49@gmail.com>
2017-02-20 01:50:48 +0200

    add array menu

---
+++ menu.php
@@ -0,0 +1,10 @@
+<?php ## Меню.
+
+$menu = array(
+    "point1"=>array("title" => "Главная", "link" => "http://vk.com", "active" => "true", "menu_type" => "top", "position" => "1"),
+    "point2"=>array("title" => "О нас", "link" => "http://vk.com", "active" => "true",  "menu_type" => "top", "position" => "0"),
+    "point3"=>array("title" => "Новости", "link" => "http://vk.com", "active" => "true", "menu_type" => "top", "position" => "3"),
+    "point4"=>array("title" => "Блог", "link" => "http://vk.com", "active" => "true",  "menu_type" => "top", "position" => "0"),
+    "point5"=>array("title" => "Контакты", "link" => "http://vk.com", "active" => "true",  "menu_type" => "top", "position" => "0"),
+);
+?>
