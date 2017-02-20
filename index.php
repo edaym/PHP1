325007eb070d26731f07fa388020985e01074f55
edaym <mailmike49@gmail.com>
2017-02-21 01:29:40 +0200

    add function that sorts an array of menu items ans displays items that are in "top" block

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ function show_menu($m, $type){
 
     foreach ($m as $massiv) {
 
-        if ($massiv["menu_type"]==$type) {
+        if ($massiv["menu_type"]==[$type]) {
             echo '<a href="' . $massiv["link"] . '", class="title">' . $massiv["title"] . '</a>' . "\n";
         }
     }
@@ -14,6 +14,16 @@ function show_menu($m, $type){
 show_menu($menu, "top");
 show_menu($menu, "left");
 show_menu($menu, "bottom");
+
+function filter_type($menu) {
+        return in_array ("top", $menu["menu_type"]);
+};
+$filt=array_filter($menu, "filter_type");
+print_r($filt);
+
 ?>
+
+
+
 
 
