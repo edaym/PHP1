670daa3c226c5314ed89e1e753383285f58151c4
edaym <mailmike49@gmail.com>
2017-02-25 01:33:38 +0200

    add 2 sort functions

---
+++ functions.php
@@ -0,0 +1,21 @@
+<?php
+
+//сортировка массива меню по возрастанию в поле position
+function usortmenu($a, $b) {
+    if ($a["position"]== $b["position"]){
+        return 0;
+    }
+    return ($a["position"] < $b["position"]) ? -1 : 1;
+};
+usort($menu, "usortmenu");
+print_r($menu);
+
+//сортировка массива меню по возрастанb. с сохранением связи ключ-значение
+function uasortmenu($a, $b) {
+    if ($a["position"]== $b["position"]){
+        return 0;
+    }
+    return ($a["position"] > $b["position"]) ? 1 : -1;
+};
+uasort($menu, "uasortmenu");
+print_r($menu);
