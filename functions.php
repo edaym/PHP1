e8ca6b62b3ee42f0d34290942b8f55940a24b7cd
edaym <mailmike49@gmail.com>
2017-02-25 11:26:53 +0200

    add 2 sort functions, include menu.php

--- functions.php
+++ functions.php
@@ -1,5 +1,5 @@
 <?php
-
+include 'menu.php';
 //сортировка массива меню по возрастанию в поле position
 function usortmenu($a, $b) {
     if ($a["position"]== $b["position"]){
@@ -10,7 +10,7 @@ function usortmenu($a, $b) {
 usort($menu, "usortmenu");
 print_r($menu);
 
-//сортировка массива меню по возрастанb. с сохранением связи ключ-значение
+//сортировка массива меню по возрастаню с сохранением связи ключ-значение
 function uasortmenu($a, $b) {
     if ($a["position"]== $b["position"]){
         return 0;
