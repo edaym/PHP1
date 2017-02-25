cb0e16db0a4a3f85a92d78cb20d91704938c8e7d
edaym <mailmike49@gmail.com>
2017-02-25 11:42:46 +0200

    add in function show_menu filter by the menu_type option

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
-include 'menu.php';
+
+/*
 function show_menu($m, $type){
  echo "<ul>";
     foreach ($m as $massiv) {
@@ -14,35 +15,32 @@ function show_menu($m, $type){
 show_menu($menu, "top");
 show_menu($menu, "left");
 show_menu($menu, "bottom");
-
+*/
+/*
 function filter_type($menu) {
         return in_array ("top", $menu["menu_type"]);
 };
 $filt=array_filter($menu, "filter_type");
-print_r($filt);
+//print_r($filt);
+*/
 
-
-
-
-function show_men($m, $type){
+include 'menu.php';
+$show_men=function($m) use ($menu){
     echo "<ul>";
- function filter_type($menu)
+ function filt($menu)
   {
     return in_array("top", $menu["menu_type"]);
-  }; //функция замыкания closure.php
+  };
+    $filt=array_filter($m, "filt");
     //функция usort ksort
-    foreach ($m as $massiv) {
-
-        if ($massiv["menu_type"]==[$type]) {
+    foreach ($filt as $massiv) {
             echo '<li><a href="' . $massiv["link"] . '", class="title">' . $massiv["title"] . '</a></li>' . "\n";
-        }
     }
     echo "</ul>";
-}
-show_men($menu, "top");
+};
+$show_men($menu);
 
 ?>
-
 
 
 
